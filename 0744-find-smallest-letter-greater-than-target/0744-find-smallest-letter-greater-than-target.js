733aeda92533a7bf0774d873931570eb90723267
Saad Elsabahy <36341653+saadelsabahy@users.noreply.github.com>
2023-06-10 17:14:58 +0200

    Time: 72 ms (14.94%), Space: 44.6 MB (10.38%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
@@ -4,9 +4,8 @@
  * @return {character}
  */
 var nextGreatestLetter = function(letters, target) {
-    const sorttedLetters = letters.sort((a,b) => a - b);
     let smallestLetter=''
-    for (let letter of sorttedLetters){
+    for (let letter of letters){
         if(letter > target && !smallestLetter){
             smallestLetter= letter
         }
